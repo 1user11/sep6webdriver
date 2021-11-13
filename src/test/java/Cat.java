15fc90914a2a7404f3ed2b5d79e791bd0e5bf2a5
1user11 <okepych@yahoo.com>
2021-11-12 18:25:12 -0600

    day3 code

--- src/test/java/Cat.java
+++ src/test/java/Cat.java
@@ -1,8 +1,8 @@
 public class Cat {
 
-        String name;
-        String breed;
-        String[] food;
+    String name;
+    String breed;
+    String[] food;
 
     public Cat(String name, String breed) {
         this.name = name;
@@ -10,9 +10,9 @@ public class Cat {
     }
 
     public Cat(String name, String breed, String[] food) {
-            this.name = name;
-            this.breed = breed;
-            this.food=food;
+        this.name = name;
+        this.breed = breed;
+        this.food = food;
     }
 
     public String[] getFood() {
@@ -24,21 +24,21 @@ public class Cat {
     }
 
     public void printCat() {
-            System.out.println("The cat's name is " + name + " and his breed is: " + breed);
-        }
+        System.out.println("The cat's name is " + name + " and his breed is: " + breed);
+    }
 
-        public void meow() {
-            System.out.println("Cat can meow");
-        }
+    public void meow() {
+        System.out.println("Cat can meow");
+    }
 
-        public void purr() {
-            System.out.println("Cat can purr");
-        }
+    public void purr() {
+        System.out.println("Cat can purr");
+    }
 
-        public boolean canEat(String meal) {
-            for (String f:  food  ) {
-                if(f.equals(meal)) return true;
-            }
-            return false;
+    public boolean canEat(String meal) {
+        for (String f : food) {
+            if (f.equals(meal)) return true;
         }
+        return false;
+    }
 }
