95504252f2283fb3fc0136e5b32f3318880742ad
1user11 <okepych@yahoo.com>
2021-11-25 00:28:19 -0600

    Day 5

--- src/test/java/AmazonSearchTest.java
+++ src/test/java/AmazonSearchTest.java
@@ -3,41 +3,39 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
-import java.math.BigDecimal;
 import java.util.List;
 
 public class AmazonSearchTest extends TestBase {
 
-
     //1. Navigate to the Amazon page
     //2. Type the query and submit
-    //3. Specify our result to get more correct search result
-    //3a.See first item from the search result
+    //3. Specify our result
+    //4. View the first result from the result list
 
+    @Parameters({"key"})
     @Test
-    public void testSearchAndVerifyResults() throws InterruptedException {
+    public void testSearchAndVerifyResults(String key) throws InterruptedException {
         navigateToAmazonPage();
-        typeQueryAndSubmit();
+        typeQueryAndSubmit(key);
         getResults();
+        assert "hunter green curtains 84 inch length".equals(key);
     }
 
     private void getResults() throws InterruptedException {
         List<WebElement> elements = driver.findElements(By.xpath(
-                "//span[contains(@class,'a-text-normal')][contains(text(),'Green')][contains(text(),'Blackout')]"));
-        Thread.sleep(3000);
-        WebElement result = elements.get(1);
+                "//span[contains(@class,'a-text-normal')][contains(text(),'Blackout')]"));
+        Thread.sleep(5000); // to have time to see the result
+        WebElement result = elements.get(0);
         result.click();
-
-        //driver.findElement(By.xpath("//span[contains(@class,'a-text-normal')][contains(text(),'Green')][contains(text(),'Blackout')][1]")).click();
-        Thread.sleep(3000); // to have time to see the result
+        Thread.sleep(5000); // to have time to see the result
     }
 
-    private void typeQueryAndSubmit() {
+    private void typeQueryAndSubmit(String key) {
         String idValue = "twotabsearchtextbox";
         WebElement textInput = driver.findElement(By.id(idValue));
-        textInput.sendKeys("green curtains 84 inch length");
+        textInput.sendKeys(key);
         textInput.submit();
     }
 
